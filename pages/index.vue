c00f9730c103e32818f2a696479b4f15717aaf81
Syamsu Rijal Efendi <syamsuefendi@gmail.com>
2023-01-16 06:16:28 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      currentTab: "active",
+      result: [],
+    };
+  },
+  mounted() {
+    console.log("Check Debugger"); // I'm text inside the component.
+  },
+};
+</script>
